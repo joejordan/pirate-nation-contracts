11d835cf7b6f32d7c48826801f025cf062cde3bc
Joe Jordan <dev@ignitesoft.com>
2023-01-02 15:32:35 -0500

    chore: add PirateNFT contracts

---
+++ src/tokens/gamenft/IGameNFT.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT LICENSE
+
+pragma solidity ^0.8.9;
+
+import {IERC721BridgableChild} from "../IERC721BridgableChild.sol";
+
+/**
+ * @title Interface for game NFTs that have stats and other properties
+ */
+interface IGameNFT is IERC721BridgableChild {
+    /**
+     * @param account Account to check hold time of
+     * @param tokenId Id of the token
+     * @return The time in seconds a given account has held a token
+     */
+    function getTimeHeld(address account, uint256 tokenId)
+        external
+        view
+        returns (uint32);
+}
